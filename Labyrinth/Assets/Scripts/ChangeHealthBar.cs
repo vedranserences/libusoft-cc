d03e3bab06150e60abad45399487c51531a4febb
Dinko[cURL] <dinkochess@gmail.com>
2017-06-04 19:05:07 +0200

    controller done

---
+++ Labyrinth/Assets/Scripts/ChangeHealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeHealthBar : MonoBehaviour {
+
+    private int spriteIndex = 4;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+
+
+    public void ChangeHBar(int increment) {
+        GameObject heart = GameObject.FindGameObjectWithTag("Health"+spriteIndex);
+        Image image = heart.GetComponent<Image>();
+        if(increment > 0){
+            image.sprite = fullHeart;
+        }else{
+            image.sprite = emptyHeart;
+        }
+        spriteIndex += increment;
+    }
+}
